e3a48edb1bfffd868a89644d8bf57c6353e16abe
Your Name <your.email@example.com>
2024-02-23 14:29:37 +0100

    new commit

--- my-project/src/components/PopularProductCard.jsx
+++ my-project/src/components/PopularProductCard.jsx
@@ -1,9 +1,11 @@
+// import IMG from '/src/assets/icons/star.svg'
+
 const PopularProductCard = ({ imgURL, name, price }) => {
   return (
     <div className="">
       <img src={imgURL} alt={name} width={250} height={250} />
       <div className="flex flex-row relative top-3">
-        <img src="src/assets/icons/star.svg" alt="star" />
+        <img src={'/star.svg'} alt="star" />
         <p>(4.5)</p>
       </div>
       <div className=" relative top-10">
